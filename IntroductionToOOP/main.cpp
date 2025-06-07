5672ec7d53b82348a77e86c9bc3de0137b819841
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2025-06-07 12:05:22 +0500

    CHECK_HomeWork

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
-#include <cmath>
+//#include <cmath>
 using namespace std;
+
+//#define CONSTRUCTORS_CHECK
+#define DISTANCE_CHECK
 
 class Point
 {
@@ -51,27 +54,49 @@ public:
 	}
 
 	//Methods
-	void print()const
+
+	// Разбор в классе
+	double distance(Point other) //other - точка, до коорой находим расстояние
 	{
-		cout << "X = " << get_x() << "\tY = " << get_y() << endl;
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+			//sqrt - Square Root
+		return distance;
 	}
 
-	double distance(Point Point)const
+	// Мой вариант
+	/*double distance(Point Point)const
 	{
 		double x1 = Point.x;
 		double y1 = Point.y;
 		return sqrt(pow(get_x() - x1, 2) + pow(get_y() - y1, 2));
+	}*/
+
+	void print()const
+	{
+		cout << "X = " << get_x() << "\tY = " << get_y() << endl;
 	}
 };
 
-double distance(Point Point1, Point Point2)
+//Решение в классе
+
+double distance(Point A, Point B)
 {
-	double x1 = Point1.get_x();
-	double x2 = Point2.get_x();
-	double y1 = Point1.get_y();
-	double y2 = Point2.get_y();
-	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	return sqrt(x_distance * x_distance + y_distance * y_distance);
 }
+
+//Решение дома
+//double distance(Point Point1, Point Point2)
+//{
+//	double x1 = Point1.get_x();
+//	double x2 = Point2.get_x();
+//	double y1 = Point1.get_y();
+//	double y2 = Point2.get_y();
+//	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+//}
 
 //#define STRUCT_POINT
 
@@ -92,6 +117,7 @@ void main()
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUCT_POINT
 
+#ifdef CONSTRUCTORS_CHECK
 	Point A;
 	//A.set_x(2);
 	//A.set_y(3);
@@ -104,13 +130,26 @@ void main()
 
 	Point C(7, 8);
 	C.print();
+#endif // CONSTRUCTORS_CHECK
 
-	cout << endl;
-	cout << "Определение расстояния до указанной точки" << endl;
-	cout << "Расстояние до указанной точки: " << A.distance(C) << endl; //т.С - заданная точкаб т.А - указанная точка
+	Point A(2, 3);
+	Point B(7, 8);
+	A.print();
+	B.print();
 
-	cout << endl;
-	cout << "Определение расстояние между двумя точками" << endl;
-	cout << "Расстояние между двумя точками: "<<distance(A, C) << endl;
+	cout << "Расстояние от точки 'A' до точки 'B': " << A.distance(B) << endl;
+	cout << "Расстояние от точки 'B' до точки 'A': " << B.distance(A) << endl;
+
+	cout << "Расстояние между точками 'A' и 'B': " << distance(A, B) << endl;
+	cout << "Расстояние между точками 'B' и 'A': " << distance(B, A) << endl;
+	
+	//Решение дома
+	//cout << endl;
+	//cout << "Определение расстояния до указанной точки" << endl;
+	//cout << "Расстояние до указанной точки: " << A.distance(C) << endl; //т.С - заданная точка; т.А - указанная точка
+
+	//cout << endl;
+	//cout << "Определение расстояние между двумя точками" << endl;
+	//cout << "Расстояние между двумя точками: "<<distance(A, C) << endl;
 
 }
